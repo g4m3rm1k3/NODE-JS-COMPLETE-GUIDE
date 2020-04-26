0f317965de851d82d5661e9c4dbf93532c5c22f8
Michael McLean <g4m3rm1k3@hotmail.com>
2020-04-26 19:27:10 -0400

    arrow functions

--- play.js
+++ play.js
@@ -5,9 +5,30 @@ const hasHobbies = true;
 // name = "Michael"    
 age = 30;
 
-function summarizeUser(userName, userAge, userHasHobby){
-  return "Name is " + userName + " age is " + userAge + " and the user has hobbies is: " + userHasHobby ;
-}
+// arrow function with =>
+const summarizeUser = (userName, userAge, userHasHobby) =>{
+  return ("Name is " + userName + " age is " + userAge + "User has hobbies: " + userHasHobby
+);
+};
+
+// const add = (a, b) =>{
+//   return a + b
+// }
+
+const add = (a,b) => a + b;
+console.log(add(1,3))
+
+const addOne = a => a + 1;
+console.log(addOne(3));
+
+// function as constant set to anonymous function
+// const summarizeUser = function(userName, userAge, userHasHobby){
+//   return "Name is " + userName + " age is " + userAge + " user has hobbies: " + userHasHobby
+// }
+
+// function summarizeUser(userName, userAge, userHasHobby){
+//   return "Name is " + userName + " age is " + userAge + " and the user has hobbies is: " + userHasHobby ;
+// }
 
 console.log(name);
 console.log(summarizeUser(name, age, hasHobbies));
