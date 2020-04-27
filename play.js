ca88752192cd4da9b447d095a15e21215a0a90cf
Michael McLean <g4m3rm1k3@hotmail.com>
2020-04-27 04:51:34 -0400

    lesson 19 destructuring

--- play.js
+++ play.js
@@ -5,21 +5,35 @@ const person = {
     console.log('Hi, I am ' + this.name);
   }
 };
-const copiedPerson = {...person};
-console.log(copiedPerson);
+
+const printName = ({name}) => {
+    console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
 
 const hobbies = ['Sports', 'Cooking'];
-// for (let hobby of hobbies){
-//   console.log(hobby);
-// }
-// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-// console.log(hobbies);
-const copiedArray = [...hobbies];
-console.log(copiedArray);
-hobbies.push('Programming');
-// console.log(hobbies);
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1);
+console.log(hobby2);
 
-const toArray = (...args) =>{
-  return args;
-};
-console.log(toArray(1,2,3,4));
+// // for (let hobby of hobbies){
+// //   console.log(hobby);
+// // }
+// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// // console.log(hobbies);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+// hobbies.push('Programming');
+// // console.log(hobbies);
+
+// const toArray = (...args) =>{
+//   return args;
+// };
+// console.log(toArray(1,2,3,4));
