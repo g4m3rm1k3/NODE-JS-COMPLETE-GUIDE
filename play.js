495449d74f5518cff7945da86d7883c293d71c32
Michael McLean <g4m3rm1k3@hotmail.com>
2020-04-29 18:29:01 -0400

    lesson 20 async and promises

--- play.js
+++ play.js
@@ -1,39 +1,27 @@
-const person = {
-  name:'Mike',
-  age: 39,
-  greet(){
-    console.log('Hi, I am ' + this.name);
-  }
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      resolve('Done!');
+    }, 1500);
+  });
+  return promise;
 };
 
-const printName = ({name}) => {
-    console.log(name);
-};
+// asynchronous code
+//call back function oldest way of handling
+setTimeout(() => {
+  console.log('Timer is done!');
+  fetchData()
+  .then(text => {
+    console.log(text);
+    return fetchData();
+  })
+  .then(text2 =>{
+    console.log(text2);
+  });
+}, 2000);
 
-printName(person);
 
-const { name, age } = person;
-console.log(name, age);
-
-// const copiedPerson = {...person};
-// console.log(copiedPerson);
-
-const hobbies = ['Sports', 'Cooking'];
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1);
-console.log(hobby2);
-
-// // for (let hobby of hobbies){
-// //   console.log(hobby);
-// // }
-// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-// // console.log(hobbies);
-// const copiedArray = [...hobbies];
-// console.log(copiedArray);
-// hobbies.push('Programming');
-// // console.log(hobbies);
-
-// const toArray = (...args) =>{
-//   return args;
-// };
-// console.log(toArray(1,2,3,4));
+// synchronous code
+console.log("Hello!");
+console.log("Hi!");
